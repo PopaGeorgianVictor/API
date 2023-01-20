90cab7621ea2e475ab597a0978080dbdd0e9a566
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-20 19:52:53 +0200

    api test

--- first_api_test.py
+++ first_api_test.py
@@ -1,9 +1,14 @@
 import requests
-import json
-import jsonpath
 
 
 # API URL
 url = "https://reqres.in/api/users?page=2"
 
 # Send Get Request
+response = requests.get(url)
+print(response)
+
+# Display Response Content
+print(response.content)
+print()
+print(response.headers)

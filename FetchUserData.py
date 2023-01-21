9768fc146d65d67b82cf22954f436b45ebdeb395
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-21 11:02:14 +0200

    api test

--- FetchUserData.py
+++ FetchUserData.py
@@ -11,5 +11,13 @@ response = requests.get(url)
 
 
 # Parse response to Json format
+
 json_response = json.loads(response.text)
 print(json_response)
+
+# Fetch value using Json Path
+
+pages = jsonpath.jsonpath(json_response,'total_pages')
+print(pages[0])
+
+assert pages[0] == 2 , 'API Error'

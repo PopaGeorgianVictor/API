b90204f26f63f2b8005e3131119d4e39cf191fb1
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-21 13:02:34 +0200

    api test

--- GET_Request/CreatingNewResource.py
+++ GET_Request/CreatingNewResource.py
@@ -1,5 +1,6 @@
 '''
 POST method it use for creating a new resource on the server
+PUT use for updating data
 
 STEPS
 
@@ -11,14 +12,39 @@ STEPS
 '''
 
 import requests
+import json
+import jsonpath
 
 # API URL
 url = "https://reqres.in/api/users"
 
-
 # Read input json from file
 file = open("C:\\Users\\popag\\OneDrive\\Desktop\\DOC\\API\\CreateUserJson.json", 'r')
 # updating to double \
-# mode to be open : read mode
+# mode to be open : read only mode
 
-json_input = file.read() # read complete content of the file
+json_input = file.read() # read complete content of the file, just a string
+
+# Parse data into json format
+request_json = json.loads(json_input)
+print(request_json) # checking if I am able to read and pars into json format
+
+# Make POST request with Json Input body
+response = requests.put(url,request_json) # giving url and data
+print(response.content)
+
+# Validating Response Code
+response1 = requests.post(url,request_json)
+assert response1.status_code == 201
+
+# Fetch Header from Response
+print(response1.headers)
+print(response1.headers.get('Content-Length'))
+
+# Parse response to json format
+response_json = json.loads(response1.text)
+
+# Pick Id using Json Path
+id = jsonpath.jsonpath(response_json, 'id')
+print(id[0]) # getting a new id every time I running because a new resource is created
+

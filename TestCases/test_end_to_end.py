78f7e3272c576d7bffbfebda946bc7f649164c3b
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-22 16:56:01 +0200

    api test

--- TestCases/test_end_to_end.py
+++ TestCases/test_end_to_end.py
@@ -10,10 +10,13 @@ import json
 import jsonpath
 
 
-def test_Add_neew_data():
+def test_Add_new_data():
     APP_URL = "https://thetestingworldapi.com/api/studentsDetails"
     file = open('D:\\selenium project\\API\\TestCases\\AddUser.json', 'r')
     request_json = json.loads(file.read())
     response = requests.post(APP_URL,request_json)
     print(response.text)
-    id = jsonpath.jsonpath(request_json(),'id')
+    id = jsonpath.jsonpath(response.json(),'id')
+    print(id[0])
+
+    tech_api_url = "https://thetestingworldapi.com/api/technicalskills"

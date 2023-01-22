06ae06dd9855776386f50331792ac7fdd104633b
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-22 12:11:05 +0200

    api test

---
+++ TestCases/test_CreateUser.py
@@ -0,0 +1,38 @@
+'''
+1. Convert TestCase into Pytest format
+2. Execute TestCase using Python
+3. Write Multiple TestCase in a File
+'''
+
+import requests
+import json
+import jsonpath
+
+# API URL
+url = "https://reqres.in/api/users"
+
+def test_create_new_user():
+    # Read input json from file
+    file = open("C:\\Users\\popag\\OneDrive\\Desktop\\DOC\\API\\CreateUserJson.json", 'r')
+    json_input = file.read()  # read complete content of the file, just a string
+    request_json = json.loads(json_input)
+    print(request_json)  # checking if I am able to read and pars into json format
+    # Make POST request with Json Input body
+    response = requests.post(url, request_json)  # giving url and data
+    print(response.content)
+    # Validating Response Code
+    assert response.status_code == 201
+
+
+def test_create_other_user():
+    # Read input json from file
+    file = open("C:\\Users\\popag\\OneDrive\\Desktop\\DOC\\API\\CreateUserJson.json", 'r')
+    json_input = file.read()  # read complete content of the file, just a string
+    request_json = json.loads(json_input)
+    # Make POST request with Json Input body
+    response = requests.post(url, request_json)  # giving url and data
+    print(response.content)
+    response_json = json.loads(response.text)
+    # Pick Id using Json Path (fetch first item in the list)
+    id = jsonpath.jsonpath(response_json, 'id')
+    print(id[0])  # getting a new id every time I running because a new resource is created

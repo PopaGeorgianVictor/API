7228723ba958fd088a8db10d0210e292a303f684
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-04 12:09:23 +0200

    api test

--- reqres/tests/test_user.py
+++ reqres/tests/test_user.py
@@ -2,33 +2,33 @@ from reqres.requests.users import *
 
 
 def test_get_user_200():
-    response = get_user(1)
+    response = get_user(11)
     assert response.status_code == 200, 'status code is not ok'
 
 
 def test_get_user_id():
-    response = get_user(1)
-    assert response.json()['data']['id'] == 1, 'id is not ok'
+    response = get_user(11)
+    assert response.json()['data']['id'] == 11, 'id is not ok'
 
 
 def test_get_user_email():
-    response = get_user(1)
-    assert response.json()['data']['email'] == 'george.bluth@reqres.in', 'email is not ok'
+    response = get_user(11)
+    assert response.json()['data']['email'] == 'george.edwards@reqres.in', 'email is not ok'
 
 
 def test_get_user_first_name():
-    response = get_user(1)
+    response = get_user(11)
     assert response.json()['data']['first_name'] == 'George', 'first name is not ok'
 
 
 def test_get_user_last_name():
-    response = get_user(1)
-    assert response.json()['data']['last_name'] == 'Bluth', 'last name is not ok'
+    response = get_user(11)
+    assert response.json()['data']['last_name'] == 'Edwards', 'last name is not ok'
 
 
 def test_get_user_avatar():
-    response = get_user(1)
-    assert response.json()['data']['avatar'] == 'https://reqres.in/img/faces/1-image.jpg', 'avatar is not ok'
+    response = get_user(11)
+    assert response.json()['data']['avatar'] == 'https://reqres.in/img/faces/11-image.jpg', 'avatar is not ok'
 
 
 def test_get_users_total():
@@ -39,15 +39,15 @@ def test_get_users_total():
 def test_get_users_totals():
     response = get_all_users(1)
     expected_user = {
-        'id': 1,
-        'email': 'george.bluth@reqres.in',
-        'first_name': 'George',
-        'last_name': 'Bluth',
-        'avatar': 'https://reqres.in/img/faces/1-image.jpg'
+            "id": 11,
+            "email": "george.edwards@reqres.in",
+            "first_name": "George",
+            "last_name": "Edwards",
+            "avatar": "https://reqres.in/img/faces/11-image.jpg"
     }
     assert response.json()['data'][0] == expected_user, 'user data is not ok'
 
 
 def test_add_user():
-    response = add_user('Andy', 'trainer')
+    response = add_user('George', 'QA')
     assert 'id' in response.json().keys(), 'user not created'

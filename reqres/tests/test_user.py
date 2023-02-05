31f71e55f47b8c250463a6dc26ba292b95dd2e23
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-05 18:44:55 +0200

    api test

--- reqres/tests/test_user.py
+++ reqres/tests/test_user.py
@@ -3,37 +3,37 @@ from reqres.requests.users import *
 
 def test_get_user_200():
     response = get_user(11)
-    assert response.status_code == 200, 'status code is not ok'
+    assert response.status_code == 200, 'status code nu este corect'
 
 
 def test_get_user_id():
     response = get_user(11)
-    assert response.json()['data']['id'] == 11, 'id is not ok'
+    assert response.json()['data']['id'] == 11, 'id-ul nu este corect'
 
 
 def test_get_user_email():
     response = get_user(11)
-    assert response.json()['data']['email'] == 'george.edwards@reqres.in', 'email is not ok'
+    assert response.json()['data']['email'] == 'george.edwards@reqres.in', 'email-ul nu este corect'
 
 
 def test_get_user_first_name():
     response = get_user(11)
-    assert response.json()['data']['first_name'] == 'George', 'first name is not ok'
+    assert response.json()['data']['first_name'] == 'George', 'first name nu este corect'
 
 
 def test_get_user_last_name():
     response = get_user(11)
-    assert response.json()['data']['last_name'] == 'Edwards', 'last name is not ok'
+    assert response.json()['data']['last_name'] == 'Edwards', 'last name nu este corect'
 
 
 def test_get_user_avatar():
     response = get_user(11)
-    assert response.json()['data']['avatar'] == 'https://reqres.in/img/faces/11-image.jpg', 'avatar is not ok'
+    assert response.json()['data']['avatar'] == 'https://reqres.in/img/faces/11-image.jpg', 'avatarul nu este corect'
 
 
 def test_get_users_totals():
     response = get_all_users(1)
-    assert response.json()['total'] == 12, 'total nr of users is not ok'
+    assert response.json()['total'] == 12, 'numarul total de utilizatori nu este corect'
 
 
 def test_get_my_user():
@@ -45,10 +45,10 @@ def test_get_my_user():
             "last_name": "Edwards",
             "avatar": "https://reqres.in/img/faces/11-image.jpg"
     }
-    assert response.json()['data'][4] == expected_user, 'user data is not ok'
+    assert response.json()['data'][4] == expected_user, 'user data nu este corect'
 
 
 def test_add_user():
     response = add_user('George', 'QA')
-    assert 'id' in response.json().keys(), 'user not created'
+    assert 'id' in response.json().keys(), 'user-ul nu a fost creat'
 

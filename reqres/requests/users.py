6de996e98698c622e041aecf0cf21c2766c12edd
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-05 18:26:44 +0200

    api test

--- reqres/requests/users.py
+++ reqres/requests/users.py
@@ -5,12 +5,9 @@ def get_user(user_id):
     response = requests.get(f'https://reqres.in/api/users/{user_id}')
     return response
 
-
-
 def get_all_users(page):
     response = requests.get(f'https://reqres.in/api/users?page={page}')
     return response
-
 
 def add_user(name, job):
     data = {

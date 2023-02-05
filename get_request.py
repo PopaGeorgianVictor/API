34ac05247b6647131798b41e5c217997787728c5
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-05 11:33:40 +0200

    api tst

--- get_request.py
+++ get_request.py
@@ -16,9 +16,8 @@ assert  response.status_code == 200 , 'Cod Invalid'
 
 # Parse response to Json format
 json_response = json.loads(response.text)
-print(json_response) # return a list
+print(json_response)
 
 # Fetch value using Json Path
 pages = jsonpath.jsonpath(json_response,'total_pages')
-print(pages)
-assert pages[0] == 2 ,'numarul nu este corect' # fetching the first value of the list
+assert pages[0] == 2 ,'numarul de pagini nu este corect'

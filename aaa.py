03c251256b799dc633fc0d2d8ddbbe412a9f857d
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-05 11:18:48 +0200

    api test

--- aaa.py
+++ aaa.py
@@ -1,5 +1,5 @@
 
-''' FETCH or GET
+''' FETCH sau GET
     1. Fetch Response Content
     2. Json Path '''
 
@@ -10,15 +10,15 @@ import jsonpath
 # API URL
 url = "https://reqres.in/api/users?page=2"
 
-# Send Get Request
+# Get Request
 response = requests.get(url)
-
+assert  response.status_code == 200 , 'Cod Invalid'
 
 # Parse response to Json format
 json_response = json.loads(response.text)
-print(json_response)# return a list
+print(json_response) # return a list
 
 # Fetch value using Json Path
 pages = jsonpath.jsonpath(json_response,'total_pages')
 print(pages)
-assert pages[0] == 2 , 'API Error' # fetching the first value of the list
+assert pages[0] == 2 ,'numarul nu este corect' # fetching the first value of the list

d3d9013da306a929f925e3c21781690bca47c850
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-10 16:10:24 +0200

    api test

--- SimpleBooks/tests/test_submit_order.py
+++ SimpleBooks/tests/test_submit_order.py
@@ -6,9 +6,7 @@ class TestSubmitOrder:
         token = generate_token()
         response = submit_order(1, "Victor Ion Popa", token)
         assert response.status_code == 201, f"Error: Status code is not valid. Expected: 201, actual: {response.status_code}"
-        assert response.json()[
-                   "created"] == True, f"Error: Order creation status is not correct. Expected: True, Actual: {response.json()['created']}"
-        assert len(response.json()[
-                       "orderId"]) > 0, f"Error: Order id is invalid. Expected lenght greater than zero. Actual length: {len(response.json()['orderId'])} "
+        assert response.json()["created"] == True, f"Error: Order creation status is not correct. Expected: True, Actual: {response.json()['created']}"
+        assert len(response.json()["orderId"]) > 0 , f"Error: Order id is invalid. Expected lenght greater than zero. Actual length: {len(response.json()['orderId'])} "
 
 

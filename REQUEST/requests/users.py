44fbec62411d792112859feafc07893895f053d7
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-12 11:22:04 +0200

    api test

--- REQUEST/requests/users.py
+++ REQUEST/requests/users.py
@@ -9,10 +9,15 @@ def get_all_users(page):
     response = requests.get(f'https://reqres.in/api/users?page={page}')
     return response
 
+
 def add_user(name, job):
     data = {
         'name': name,
         'job': job
     }
     response = requests.post(f'https://reqres.in/api/users', data)
+    return response
+
+def delete_user(user_id):
+    response = requests.delete(f'https://reqres.in/api/users/{user_id}')
     return response

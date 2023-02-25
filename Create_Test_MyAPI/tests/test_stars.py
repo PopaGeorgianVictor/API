b0e6f78bf1084cf68f8750215d9aef8b50bffe92
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-25 15:53:09 +0200

    api test

--- Create_Test_MyAPI/tests/test_stars.py
+++ Create_Test_MyAPI/tests/test_stars.py
@@ -24,3 +24,17 @@ def test_get_star_last_name():
 def test_get_star_avatar():
     response = get_single_star(6)
     assert response.json()['avatar'] == 'https://deadline.com/wp-content/uploads/2022/12/Celine-Dion.jpg', 'is not my star avatar'
+
+def test_check_my_star():
+    response = get_all_stars()
+    expected_star = {
+        "id" : 1,
+        "email": "popa.georgian93@gamil.com",
+        "first_name": "Georgian",
+        "last_name": "Popa",
+        "avatar": "https://npg.si.edu/sites/default/files/styles/grid_wider/public/promotion/npg-npg_87_43.jpg?itok=pWuMxYem"
+    }
+
+    assert response.json()[0] == expected_star, 'my details star is not gorgeous'
+
+

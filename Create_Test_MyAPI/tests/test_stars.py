16cdd86ed6bdd89a938a0045dae568793c60ee3a
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-02-25 12:22:06 +0200

    api test

--- Create_Test_MyAPI/tests/test_stars.py
+++ Create_Test_MyAPI/tests/test_stars.py
@@ -1,6 +1,6 @@
 
 from Create_Test_MyAPI.requests.stars import *
 
-def test_get_star_200():
+def test_get_my_star():
     response = get_single_star(1)
     assert response.status_code == 200, 'my star is not available'
